0d9d1ac3ac2a5dab619a6b95b3ad0574ca9247a3
Kay Anar <github@anars.com>
2016-01-04 22:27:36 -0500

    initial commit

---
+++ source/com/anars/brainfuckjc/BrainfuckJC.java
@@ -0,0 +1,14 @@
+package com.anars.brainfuckjc;
+
+public class BrainfuckJC
+{
+  public BrainfuckJC()
+  {
+    super();
+  }
+
+  public static void main(String[] args)
+  {
+    BrainfuckJC brainfuckJC = new BrainfuckJC();
+  }
+}
